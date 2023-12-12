bbde0404f0f3f8e251304c2c329de9cae2f21b7f
Luca De Luigi <lucadeluigi91@gmail.com>
2023-12-12 09:06:56 +0100

    feat: compute num_classes automatically

--- shape_generation/export_embeddings.py
+++ shape_generation/export_embeddings.py
@@ -10,21 +10,19 @@ def main() -> None:
     inr_embeddings_root = Path(sys.argv[1])
     out_root = Path(sys.argv[2])
     out_root.mkdir(parents=True, exist_ok=True)
-    num_classes = 40
 
     embeddings_paths = list(inr_embeddings_root.glob("*.h5"))
-
     embeddings = {}
-    for cls in range(num_classes):
-        embeddings[cls] = []
 
     for path in progress_bar(embeddings_paths, "Extracting embeddings"):
         with h5py.File(path, "r") as f:
             embedding = np.array(f.get("embedding"))
             class_id = np.array(f.get("class_id")).item()
+            if class_id not in embeddings:
+                embeddings[class_id] = []
             embeddings[class_id].append(embedding)
 
-    for class_id in progress_bar(range(num_classes), "Saving embeddings"):
+    for class_id in progress_bar(list(embeddings.keys()), "Saving embeddings"):
         path_out = out_root / f"embeddings_{class_id}.npz"
         stacked_embeddings = np.stack(embeddings[class_id])
         np.savez_compressed(path_out, embeddings=stacked_embeddings)
